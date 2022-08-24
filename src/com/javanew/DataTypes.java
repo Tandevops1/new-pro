a8c260b48fde133756fedc905e524d4c83aa274a
Tanha Mim <tanhaqa02@gmail.com>
2022-08-24 14:46:51 -0400

    this is my 1st commmit

---
+++ src/com/javanew/DataTypes.java
@@ -0,0 +1,33 @@
+package com.javanew;
+
+public class DataTypes {
+
+
+    public static void main(String[] args) {
+
+
+    System.out.println("I want to go school");
+        //java types 2 types
+        //1.primitive data
+        //2.non-primitive data
+
+
+        //1.byte
+        //2.shory
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+
+
+        byte a = 12;
+
+
+
+
+
+
+    }
+}
